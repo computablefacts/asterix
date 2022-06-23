1f86d9b99c20064468cd395ed67d4db5f521980b
csavelief <csavelief@computablefacts.com>
2022-06-23 16:35:39 +0200

    TfIdfVectorizer - 100% test coverage

---
+++ src/com/computablefacts/asterix/ml/TfIdfVectorizer.java
@@ -0,0 +1,76 @@
+package com.computablefacts.asterix.ml;
+
+import com.computablefacts.asterix.SpanSequence;
+import com.google.common.base.Preconditions;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset;
+import com.google.errorprone.annotations.CheckReturnValue;
+import com.google.errorprone.annotations.Var;
+import java.util.List;
+import java.util.function.Function;
+
+@CheckReturnValue
+final public class TfIdfVectorizer implements Function<SpanSequence, FeatureVector> {
+
+  private final LoadingCache<String, String> cache_;
+  private final Vocabulary vocabulary_;
+  private List<Integer> indices_;
+
+  public TfIdfVectorizer(Vocabulary vocabulary) {
+    this(vocabulary, 10000);
+  }
+
+  public TfIdfVectorizer(Vocabulary vocabulary, int maxCacheSize) {
+
+    Preconditions.checkNotNull(vocabulary, "vocabulary should not be null");
+    Preconditions.checkArgument(maxCacheSize > 0, "maxCacheSize must be > 0");
+
+    vocabulary_ = vocabulary;
+    cache_ = CacheBuilder.newBuilder().maximumSize(maxCacheSize)
+        .build(new CacheLoader<String, String>() {
+          @Override
+          public String load(String term) {
+            return Vocabulary.normalize(term);
+          }
+        });
+  }
+
+  @Override
+  public FeatureVector apply(SpanSequence spans) {
+
+    Preconditions.checkNotNull(spans, "spans should not be null");
+
+    Multiset<String> counts = HashMultiset.create();
+    spans.forEach(span -> counts.add(cache_.getUnchecked(span.text())));
+
+    FeatureVector vector;
+
+    if (indices_ == null) {
+
+      vector = new FeatureVector(vocabulary_.size() - 1 /* ignore UNK */);
+
+      for (int i = 0; i < vocabulary_.size() - 1 /* ignore UNK */; i++) {
+        int idx = i + 1;
+        vector.set(i, vocabulary_.tfIdf(idx, counts.count(vocabulary_.term(idx))));
+      }
+    } else {
+      
+      vector = new FeatureVector(indices_.size());
+      @Var int i = 0;
+
+      for (int idx : indices_) {
+        vector.set(i++, vocabulary_.tfIdf(idx, counts.count(vocabulary_.term(idx))));
+      }
+    }
+
+    vector.normalizeUsingEuclideanNorm();
+    return vector;
+  }
+
+  public void subsetOfVocabularyConsidered(List<Integer> indices) {
+    indices_ = indices;
+  }
+}
---
+++ test/com/computablefacts/asterix/ml/TfIdfVectorizerTest.java
@@ -0,0 +1,46 @@
+package com.computablefacts.asterix.ml;
+
+import com.computablefacts.asterix.Span;
+import com.computablefacts.asterix.View;
+import com.google.common.collect.Lists;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TfIdfVectorizerTest {
+
+  @Test
+  public void testVectorize() {
+
+    TextNormalizer normalizer = new TextNormalizer(true);
+    View<List<String>> tokens = View.of(sentences()).map(normalizer).map(new TextTokenizer())
+        .map(spans -> View.of(spans).map(Span::text).toList());
+
+    Vocabulary vocabulary = Vocabulary.of(tokens, 0.01, 0.99, 100);
+    TfIdfVectorizer vectorizer = new TfIdfVectorizer(vocabulary);
+
+    View.of(sentences()).map(normalizer).map(new TextTokenizer()).map(vectorizer)
+        .forEachRemaining(vector -> Assert.assertEquals(vocabulary.size() - 1, vector.length()));
+  }
+
+  @Test
+  public void testVectorizeOnSubsetOfVocabulary() {
+
+    TextNormalizer normalizer = new TextNormalizer(true);
+    View<List<String>> tokens = View.of(sentences()).map(normalizer).map(new TextTokenizer())
+        .map(spans -> View.of(spans).map(Span::text).toList());
+
+    Vocabulary vocabulary = Vocabulary.of(tokens, 0.01, 0.99, 100);
+    TfIdfVectorizer vectorizer = new TfIdfVectorizer(vocabulary);
+    vectorizer.subsetOfVocabularyConsidered(Lists.newArrayList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
+    View.of(sentences()).map(normalizer).map(new TextTokenizer()).map(vectorizer)
+        .forEachRemaining(vector -> Assert.assertEquals(10, vector.length()));
+  }
+
+  private List<String> sentences() {
+    return Lists.newArrayList("Welcome to Yahoo!, the world’s most visited home page.",
+        "Quickly find what you’re searching for, get in touch with friends and stay in-the-know with the latest news and information.",
+        "CloudSponge provides an interface to easily enable your users to import contacts from a variety of the most popular webmail services including Yahoo, Gmail and Hotmail/MSN as well as popular desktop address books such as Mac Address Book and Outlook.");
+  }
+}
