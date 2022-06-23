019b888152566eaf04af8f48f823fa24036f35a7
csavelief <csavelief@computablefacts.com>
2022-06-23 16:54:33 +0200

    Shuffler - 100% test coverage

---
+++ test/com/computablefacts/asterix/ml/ShufflerTest.java
@@ -0,0 +1,24 @@
+package com.computablefacts.asterix.ml;
+
+import com.computablefacts.asterix.DocumentTest;
+import com.computablefacts.asterix.View;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ShufflerTest {
+
+  @Test
+  public void testShuffleTexts() {
+
+    List<String> textsOrdered = DocumentTest.papers().map(doc -> (String) doc.text()).toList();
+
+    Shuffler<String> shuffler = new Shuffler<>();
+    List<String> textsShuffled = DocumentTest.papers().map(doc -> (String) doc.text())
+        .partition(100).map(shuffler).flatten(View::of).toList();
+
+    Assert.assertEquals(textsOrdered, textsOrdered);
+    Assert.assertEquals(textsShuffled, textsShuffled);
+    Assert.assertNotEquals(textsShuffled, textsOrdered);
+  }
+}
