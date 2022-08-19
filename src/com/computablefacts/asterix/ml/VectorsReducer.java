87c9af3b84aa1c487b5ca3875e425f6c2bde04ce
csavelief <csavelief@computablefacts.com>
2022-08-19 17:44:32 +0200

    VectorsReducer - Add comment

--- src/com/computablefacts/asterix/ml/VectorsReducer.java
+++ src/com/computablefacts/asterix/ml/VectorsReducer.java
@@ -9,6 +9,10 @@ import java.util.List;
 import java.util.Set;
 import java.util.function.Function;
 
+/**
+ * Reduce a list of vectors by removing entries that are zeroes across all vectors. The reduction is
+ * computed on the first batch of vectors seen and then applied on all the subsequents batches.
+ */
 @CheckReturnValue
 final public class VectorsReducer implements Function<List<FeatureVector>, List<FeatureVector>> {
 
