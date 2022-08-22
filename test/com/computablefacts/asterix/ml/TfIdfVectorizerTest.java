585d501f3348c2ac2862d8f2fdcb4d686f0d5203
csavelief <csavelief@computablefacts.com>
2022-08-22 18:26:38 +0200

    TfIdfVectorizer - Test the output vectors

--- test/com/computablefacts/asterix/ml/TfIdfVectorizerTest.java
+++ test/com/computablefacts/asterix/ml/TfIdfVectorizerTest.java
@@ -19,8 +19,23 @@ public class TfIdfVectorizerTest {
     Vocabulary vocabulary = Vocabulary.of(tokens, 0.01, 0.99, 100);
     TfIdfVectorizer vectorizer = new TfIdfVectorizer(vocabulary, true);
 
-    View.of(sentences()).map(normalizer).map(new TextTokenizer()).map(vectorizer)
-        .forEachRemaining(vector -> Assert.assertEquals(vocabulary.size() - 1, vector.length()));
+    List<FeatureVector> vectors = View.of(sentences()).map(normalizer).map(new TextTokenizer())
+        .map(vectorizer).toList();
+
+    Assert.assertEquals(vocabulary.size() - 1, vectors.get(0).length());
+    Assert.assertEquals(
+        "[0.0, 0.0, 0.0, 0.4175666238781924, 0.0, 0.0, 0.4175666238781924, 0.4175666238781924, 0.3175701804283441, 0.0, 0.4175666238781924, 0.3175701804283441, 0.0, 0.3175701804283441]",
+        vectors.get(0).toString());
+
+    Assert.assertEquals(vocabulary.size() - 1, vectors.get(1).length());
+    Assert.assertEquals(
+        "[0.4614620795549112, 0.0, 0.4614620795549112, 0.0, 0.0, 0.4614620795549112, 0.0, 0.2307310397774556, 0.17547690294787652, 0.0, 0.0, 0.17547690294787652, 0.35095380589575303, 0.35095380589575303]",
+        vectors.get(1).toString());
+
+    Assert.assertEquals(vocabulary.size() - 1, vectors.get(2).length());
+    Assert.assertEquals(
+        "[0.0, 0.38455284165612813, 0.0, 0.19227642082806407, 0.38455284165612813, 0.0, 0.19227642082806407, 0.0, 0.14623117405163255, 0.5768292624841922, 0.38455284165612813, 0.14623117405163255, 0.2924623481032651, 0.14623117405163255]",
+        vectors.get(2).toString());
   }
 
   @Test
@@ -34,8 +49,23 @@ public class TfIdfVectorizerTest {
     TfIdfVectorizer vectorizer = new TfIdfVectorizer(vocabulary, true);
     vectorizer.subsetOfVocabularyConsidered(Lists.newArrayList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
 
-    View.of(sentences()).map(normalizer).map(new TextTokenizer()).map(vectorizer)
-        .forEachRemaining(vector -> Assert.assertEquals(10, vector.length()));
+    List<FeatureVector> vectors = View.of(sentences()).map(normalizer).map(new TextTokenizer())
+        .map(vectorizer).toList();
+
+    Assert.assertEquals(10, vectors.get(0).length());
+    Assert.assertEquals(
+        "[0.0, 0.0, 0.0, 0.5286346066596935, 0.0, 0.0, 0.5286346066596935, 0.5286346066596935, 0.4020402441612698, 0.0]",
+        vectors.get(0).toString());
+
+    Assert.assertEquals(10, vectors.get(1).length());
+    Assert.assertEquals(
+        "[0.5427573398831147, 0.0, 0.5427573398831147, 0.0, 0.0, 0.5427573398831147, 0.0, 0.27137866994155735, 0.2063904733987656, 0.0]",
+        vectors.get(1).toString());
+
+    Assert.assertEquals(10, vectors.get(2).length());
+    Assert.assertEquals(
+        "[0.0, 0.45200308932281713, 0.0, 0.22600154466140857, 0.45200308932281713, 0.0, 0.22600154466140857, 0.0, 0.17188000000724263, 0.6780046339842257]",
+        vectors.get(2).toString());
   }
 
   private List<String> sentences() {
