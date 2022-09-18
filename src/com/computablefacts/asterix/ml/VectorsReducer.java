a88c8d7a05eb04c7708d2be62f45ca44388fcca2
csavelief <csavelief@computablefacts.com>
2022-09-18 22:28:29 +0200

    VectorsReducer - Backport reduction using correlation between features

--- src/com/computablefacts/asterix/ml/VectorsReducer.java
+++ src/com/computablefacts/asterix/ml/VectorsReducer.java
@@ -1,6 +1,9 @@
 package com.computablefacts.asterix.ml;
 
+import com.computablefacts.asterix.View;
 import com.google.common.base.Preconditions;
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Table;
 import com.google.errorprone.annotations.CheckReturnValue;
 import com.google.errorprone.annotations.Var;
 import java.util.ArrayList;
@@ -9,17 +12,69 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.function.Function;
+import smile.stat.hypothesis.CorTest;
 
-/**
- * Reduce a list of vectors by removing entries that are zeroes across all vectors. The reduction is computed on the
- * first batch of vectors seen and then applied on all the subsequents batches.
- */
 @CheckReturnValue
 final public class VectorsReducer implements Function<List<FeatureVector>, List<FeatureVector>> {
 
+  private final eCorrelation correlation_;
   private List<Integer> commonNonZeroEntries_ = null;
 
+  /**
+   * Reduce a list of vectors by removing entries that are zeroes across all vectors. The reduction is computed on the
+   * first batch of vectors seen and then applied on all the subsequents batches.
+   */
   public VectorsReducer() {
+    correlation_ = null;
+  }
+
+  /**
+   * Reduce a list of vectors by removing features that are highly correlated. The reduction is computed on the first
+   * batch of vectors seen and then applied on all the subsequents batches.
+   */
+  public VectorsReducer(eCorrelation correlation) {
+    correlation_ = correlation;
+  }
+
+  public static Table<Integer, Integer, CorTest> correlations(List<FeatureVector> vectors, eCorrelation correlation) {
+
+    Preconditions.checkNotNull(vectors, "vectors should not be null");
+    Preconditions.checkNotNull(correlation, "correlation should not be null");
+
+    // Transpose
+    int length = vectors.get(0).length();
+    List<double[]> matrix = new ArrayList<>(length);
+
+    for (int i = 0; i < length; i++) {
+
+      double[] vector = new double[vectors.size()];
+
+      for (int j = 0; j < vectors.size(); j++) {
+        vector[j] = vectors.get(j).get(i);
+      }
+
+      matrix.add(vector);
+    }
+
+    // Compute correlation coefficient between each token
+    Table<Integer, Integer, CorTest> correlations = HashBasedTable.create();
+
+    for (int i = 0; i < matrix.size(); i++) {
+      for (int j = 0; j < i + 1; j++) {
+
+        double[] v1 = matrix.get(i);
+        double[] v2 = matrix.get(j);
+
+        if (eCorrelation.KENDALL.equals(correlation)) {
+          correlations.put(i, j, CorTest.kendall(v1, v2));
+        } else if (eCorrelation.SPEARMAN.equals(correlation)) {
+          correlations.put(i, j, CorTest.spearman(v1, v2));
+        } else { // PEARSON
+          correlations.put(i, j, CorTest.pearson(v1, v2));
+        }
+      }
+    }
+    return correlations;
   }
 
   @Override
@@ -28,15 +83,33 @@ final public class VectorsReducer implements Function<List<FeatureVector>, List<
     Preconditions.checkNotNull(vectors, "vectors should not be null");
 
     if (commonNonZeroEntries_ == null) {
+      if (correlation_ != null) {
 
-      Set<Integer> commonNonZeroEntries = new HashSet<>(vectors.get(0).nonZeroEntries());
+        Set<Integer> nonCorrelatedEntries = View.range(0, vectors.get(0).length()).toSet();
 
-      for (int i = 1; i < vectors.size(); i++) {
-        commonNonZeroEntries.addAll(vectors.get(i).nonZeroEntries());
+        Table<Integer, Integer, CorTest> correlations = correlations(vectors, correlation_);
+        correlations.cellSet().removeIf(cell -> {
+          boolean isCorrelated =
+              cell.getRowKey() > cell.getColumnKey() && cell.getValue().cor >= 0.7 /* strong correlation */;
+          if (isCorrelated) {
+            nonCorrelatedEntries.remove(cell.getColumnKey());
+          }
+          return isCorrelated;
+        });
+
+        commonNonZeroEntries_ = new ArrayList<>(nonCorrelatedEntries);
+        Collections.sort(commonNonZeroEntries_);
+      } else {
+
+        Set<Integer> nonZeroEntries = new HashSet<>(vectors.get(0).nonZeroEntries());
+
+        for (int i = 1; i < vectors.size(); i++) {
+          nonZeroEntries.addAll(vectors.get(i).nonZeroEntries());
+        }
+
+        commonNonZeroEntries_ = new ArrayList<>(nonZeroEntries);
+        Collections.sort(commonNonZeroEntries_);
       }
-
-      commonNonZeroEntries_ = new ArrayList<>(commonNonZeroEntries);
-      Collections.sort(commonNonZeroEntries_);
     }
     if (commonNonZeroEntries_.isEmpty()) {
       return vectors;
@@ -55,5 +128,26 @@ final public class VectorsReducer implements Function<List<FeatureVector>, List<
       newVectors.add(newVector);
     }
     return newVectors;
+  }
+
+  /**
+   * It ranges from -1 to 1, -1 being a perfect negative correlation and +1 being a perfect positive correlation.
+   * <p>
+   * The Pearson product-moment correlation is one of the most commonly used correlations in statistics. It’s a measure
+   * of the strength and the direction of a linear relationship between two variables.
+   * <p>
+   * The nice thing about the Spearman correlation is that relies on nearly all the same assumptions as the pearson
+   * correlation, but it doesn't rely on normality, and your data can be ordinal as well. Thus, it’s a non-parametric
+   * test. More on the spearman correlation here, http://www.statstutor.ac.uk/resources/uploaded/spearmans.pdf, and on
+   * parametric vs. non-parametric here, http://www.oxfordmathcenter.com/drupal7/node/246.
+   * <p>
+   * The Kendall correlation is similar to the spearman correlation in that it is non-parametric. It can be used with
+   * ordinal or continuous data. It is a statistic of dependence between two variables.
+   * <p>
+   * Note that Spearman is computed on ranks and so depicts monotonic relationships while Pearson is on true values and
+   * depicts linear relationships. If Spearman > Pearson the correlation is monotonic but not linear.
+   */
+  public enum eCorrelation {
+    PEARSON, SPEARMAN, KENDALL
   }
 }
--- test/com/computablefacts/asterix/ml/VectorsReducerTest.java
+++ test/com/computablefacts/asterix/ml/VectorsReducerTest.java
@@ -1,5 +1,6 @@
 package com.computablefacts.asterix.ml;
 
+import com.computablefacts.asterix.ml.VectorsReducer.eCorrelation;
 import com.google.common.collect.Lists;
 import java.util.List;
 import org.junit.Assert;
@@ -37,5 +38,87 @@ public class VectorsReducerTest {
 
     Assert.assertEquals(new FeatureVector(new double[]{0.640, 0.0, 0.845}), newVectors.get(0));
     Assert.assertEquals(new FeatureVector(new double[]{0.0, 3.0, 5.0}), newVectors.get(1));
+  }
+
+  @Test
+  public void testReduceUsingKendallCorrelationWhenFeaturesAreNotCorrelated() {
+
+    double[] vec1 = new double[]{1.0, 8.0};
+    double[] vec2 = new double[]{2.0, 7.0};
+    double[] vec3 = new double[]{3.0, 6.0};
+    double[] vec4 = new double[]{4.0, 5.0};
+    double[] vec5 = new double[]{5.0, 4.0};
+    double[] vec6 = new double[]{6.0, 3.0};
+    double[] vec7 = new double[]{7.0, 2.0};
+    double[] vec8 = new double[]{8.0, 1.0};
+
+    FeatureVector vector1 = new FeatureVector(vec1);
+    FeatureVector vector2 = new FeatureVector(vec2);
+    FeatureVector vector3 = new FeatureVector(vec3);
+    FeatureVector vector4 = new FeatureVector(vec4);
+    FeatureVector vector5 = new FeatureVector(vec5);
+    FeatureVector vector6 = new FeatureVector(vec6);
+    FeatureVector vector7 = new FeatureVector(vec7);
+    FeatureVector vector8 = new FeatureVector(vec8);
+
+    List<FeatureVector> vectors = Lists.newArrayList(vector1, vector2, vector3, vector4, vector5, vector6, vector7,
+        vector8);
+    List<FeatureVector> newVectors = new VectorsReducer(eCorrelation.KENDALL).apply(vectors);
+
+    Assert.assertEquals(vector1, newVectors.get(0));
+    Assert.assertEquals(vector2, newVectors.get(1));
+    Assert.assertEquals(vector3, newVectors.get(2));
+    Assert.assertEquals(vector4, newVectors.get(3));
+    Assert.assertEquals(vector5, newVectors.get(4));
+    Assert.assertEquals(vector6, newVectors.get(5));
+    Assert.assertEquals(vector7, newVectors.get(6));
+    Assert.assertEquals(vector8, newVectors.get(7));
+  }
+
+  @Test
+  public void testReduceUsingKendallCorrelationWhenFeaturesAreCorrelated() {
+
+    double[] vec1 = new double[]{1.0, 1.0};
+    double[] vec2 = new double[]{2.0, 2.0};
+    double[] vec3 = new double[]{3.0, 3.0};
+    double[] vec4 = new double[]{4.0, 5.0};
+    double[] vec5 = new double[]{5.0, 4.0};
+    double[] vec6 = new double[]{6.0, 7.0};
+    double[] vec7 = new double[]{7.0, 6.0};
+    double[] vec8 = new double[]{8.0, 8.0};
+    double[] vec9 = new double[]{9.0, 10.0};
+    double[] vec10 = new double[]{10.0, 9.0};
+    double[] vec11 = new double[]{11.0, 11.0};
+    double[] vec12 = new double[]{12.0, 12.0};
+
+    FeatureVector vector1 = new FeatureVector(vec1);
+    FeatureVector vector2 = new FeatureVector(vec2);
+    FeatureVector vector3 = new FeatureVector(vec3);
+    FeatureVector vector4 = new FeatureVector(vec4);
+    FeatureVector vector5 = new FeatureVector(vec5);
+    FeatureVector vector6 = new FeatureVector(vec6);
+    FeatureVector vector7 = new FeatureVector(vec7);
+    FeatureVector vector8 = new FeatureVector(vec8);
+    FeatureVector vector9 = new FeatureVector(vec9);
+    FeatureVector vector10 = new FeatureVector(vec10);
+    FeatureVector vector11 = new FeatureVector(vec11);
+    FeatureVector vector12 = new FeatureVector(vec12);
+
+    List<FeatureVector> vectors = Lists.newArrayList(vector1, vector2, vector3, vector4, vector5, vector6, vector7,
+        vector8, vector9, vector10, vector11, vector12);
+    List<FeatureVector> newVectors = new VectorsReducer(eCorrelation.KENDALL).apply(vectors);
+
+    Assert.assertEquals(new FeatureVector(new double[]{1.0}), newVectors.get(0));
+    Assert.assertEquals(new FeatureVector(new double[]{2.0}), newVectors.get(1));
+    Assert.assertEquals(new FeatureVector(new double[]{3.0}), newVectors.get(2));
+    Assert.assertEquals(new FeatureVector(new double[]{5.0}), newVectors.get(3));
+    Assert.assertEquals(new FeatureVector(new double[]{4.0}), newVectors.get(4));
+    Assert.assertEquals(new FeatureVector(new double[]{7.0}), newVectors.get(5));
+    Assert.assertEquals(new FeatureVector(new double[]{6.0}), newVectors.get(6));
+    Assert.assertEquals(new FeatureVector(new double[]{8.0}), newVectors.get(7));
+    Assert.assertEquals(new FeatureVector(new double[]{10.0}), newVectors.get(8));
+    Assert.assertEquals(new FeatureVector(new double[]{9.0}), newVectors.get(9));
+    Assert.assertEquals(new FeatureVector(new double[]{11.0}), newVectors.get(10));
+    Assert.assertEquals(new FeatureVector(new double[]{12.0}), newVectors.get(11));
   }
 }
